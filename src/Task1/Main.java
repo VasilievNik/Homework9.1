4b8cc8a3c0ddf2662b0c78e33907821bd2261b19
nikit <nikita.veselyev@gmail.com>
2022-05-16 11:44:41 +0300

    2nd Commit created Book, Author, Main code

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -3,6 +3,15 @@ package Task1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Author author1 = new Author("Gneg", "Johnson");
+        Book book1 = new Book(1999, "Life and living", author1);
+
+        Author author2 = new Author("Jimmy", "Smith");
+        Book book2 = new Book(2023, "I hate my computer", author2);
+
+        book1.getBookAllInfo();
+        book2.getBookAllInfo();
+        book2.setPublishDate(2021);
+        book2.getBookAllInfo();
     }
 }
